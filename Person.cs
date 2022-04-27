384884111974ee1463123602f763f6372bf9c618
Sławomir Świętoniowski <slawomir.swietoniowski@codecool.com>
2022-04-27 14:11:33 +0200

    Przykład "skończony"

---
+++ Person.cs
@@ -0,0 +1,70 @@
+﻿namespace HelloOOP
+{
+    public class Person
+    {
+        public static int numberOfPeople = 0;
+
+        private string _firstName;
+
+        public string FirstName
+        {
+            get 
+            { 
+                return _firstName; 
+            }
+            set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    _firstName = "<NIEZNANE>";
+                }
+                _firstName = value; 
+            }
+        }
+
+        public string lastName;
+        public int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            Console.WriteLine("Konstruktor z parametrami");
+            
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                //throw new ArgumentException(nameof(lastName));
+                lastName = "<NIEZNANE>";
+            }
+            if (age < 0)
+            {
+                //throw new ArgumentException(nameof(age));
+                age = 0;
+            }
+
+            SetFirstName(firstName);
+            this.lastName = lastName;
+            this.age = age;
+
+            numberOfPeople++;
+        }
+
+        public string GetFirstName()
+        {
+            return _firstName;
+        }
+
+        public void SetFirstName(string firstName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                //throw new ArgumentException(nameof(firstName));
+                firstName = "<NIEZNANE>";
+            }
+            this._firstName = firstName;
+        }
+
+        public void PrintPersonInfo()
+        {
+            Console.WriteLine($"{_firstName} {lastName} is {age} years old.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,66 @@
-﻿Console.WriteLine("Hello, OOP!");
+﻿namespace HelloOOP;
+
+public class Program
+{
+    public static void Main()
+    {
+        //string firstName = "John";
+        //string lastName = "Doe";
+        //int age = 30;
+        Person person = new Person("John", "Doe", 30);
+        Console.WriteLine($"Liczba osób: {Person.numberOfPeople}");
+        //person.firstName = "John";
+        //person.lastName = "Doe";
+        //person.age = 30;
+        //person.PrintPersonInfo();
+
+        //string firstName2 = "Jane";
+        //string lastName2 = "Smith";
+        //int age2 = 20;
+        Person person2 = new Person("Jane", "Smith", 20);
+        Console.WriteLine($"Liczba osób: {Person.numberOfPeople}");
+        //person2.firstName = "Jane";
+        //person2.lastName = "Smith";
+        //person2.age = 20;
+
+        //PrintPersonInfo(firstName, lastName, age);
+        //PrintPersonInfo(firstName, lastName2, age2);
+        //Person.PrintPersonInfo(person);
+        //Person.PrintPersonInfo(person2);
+        person.PrintPersonInfo();
+        person2.PrintPersonInfo();
+
+        //person.firstName = "";
+        person.lastName = "";
+        person.age = -1;
+
+        person.PrintPersonInfo();
+
+        Person wrongPerson = new Person("", "", -1);
+        wrongPerson.PrintPersonInfo();
+        wrongPerson.SetFirstName("");
+        Console.WriteLine(wrongPerson.GetFirstName());
+        //wrongPerson.firstName = "";
+        wrongPerson.PrintPersonInfo();
+        wrongPerson.SetFirstName("John");
+        Console.WriteLine(wrongPerson.GetFirstName());
+        wrongPerson.FirstName = "Jane";
+        Console.WriteLine(wrongPerson.FirstName);
+        Console.WriteLine($"Liczba osób: {Person.numberOfPeople}");
+
+        //Math.Cos(1.0);
+
+        //Random rnd = new Random();
+        //rnd.Next();
+    }
+
+    public static void PrintPersonInfo(string firstName, string lastName, int age)
+    {
+        Console.WriteLine($"{firstName} {lastName} is {age} years old.");
+    }
+
+    //public static void PrintPersonInfo(Person person)
+    //{
+    //    Console.WriteLine($"{person.firstName} {person.lastName} is {person.age} years old.");
+    //}
+}
